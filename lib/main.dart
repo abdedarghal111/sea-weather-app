a3b6361a010073847a0c6f557352faf7edfbce26
Abderrahim Darghal Belkacemi <abdedarghal111@gmail.com>
2026-08-01 12:15:27 +0200

    feat: wire up Sea Wether app entry point
    Replace the Hello World home screen with SpotsListScreen and rename
    the app title.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/spots_list_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,12 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Tiempo de playa',
+      theme: ThemeData(colorSchemeSeed: Colors.blue, useMaterial3: true),
+      home: const SpotsListScreen(),
     );
   }
 }
